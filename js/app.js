a68619edd8ca815d383bf7d404ace5789255f1fa
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-05 17:35:00 +0100

    ADD unique list view

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-const num = prompt('Quante email vuoi generare?')
+let num = prompt('Quante email vuoi generare?')
 const container = document.getElementById("email-container")
 const btn = document.getElementById("regen")
 let emails = []
 
-generateList = function () {
-    for (let i = 0; i < num; i++) {
+function generateList(num) {
+    container.classList.add("d-none")
+    for (let i = 1; i <= num; i++) {
         axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res) => {
@@ -14,14 +15,20 @@ generateList = function () {
             .catch((err) => {
                 console.log('Errore')
             })
+            .then(() => {
+                if (emails.length == num) {
+                    container.classList.remove("d-none")
+                }
+            })
     }
     console.log(emails)
 }
 
-generateList();
+generateList(num);
 
 btn.addEventListener("click", function () {
     container.innerHTML = '';
     emails = []
-    generateList()
+    num = prompt('Quante email vuoi generare?')
+    generateList(num)
 })
